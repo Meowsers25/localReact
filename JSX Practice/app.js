4f5f314ef989dd291895c8c381653225a635239b
Chris <c.lamothe25@gmail.com>
2019-03-10 15:07:47 -0400

    Initial commit; Scrimba React lessons done locally

---
+++ JSX Practice/app.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+
+// ReactDOM.render(<h1>Hello, World</h1>, root);
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  root
+);
