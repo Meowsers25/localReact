01fc926591f74f368bb66717f513315aab825fc5
Chris <c.lamothe25@gmail.com>
2019-03-13 14:02:23 -0400

    Styling with classes; need to use className; style.css is in the public folder

---
+++ style_with_classes/src/components/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Footer() {
+  return <footer className="foot">This is the footer</footer>;
+}
+export default Footer;
---
+++ style_with_classes/src/components/MainContent.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function MainContent() {
+  return <p className="content">This is some main content</p>;
+}
+export default MainContent;
