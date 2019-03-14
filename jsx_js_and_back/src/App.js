71a830ed1a49231b482f477cbdf1536d70f5cb7c
Chris <c.lamothe25@gmail.com>
2019-03-14 10:30:45 -0400

    Adding inline style; Using styles object; Changing style dynamically

--- jsx_js_and_back/src/App.js
+++ jsx_js_and_back/src/App.js
@@ -5,15 +5,35 @@ const App = () => {
   const hours = date.getHours();
   let timeOfDay;
 
+  // styles object
+  const styles = {
+    fontSize: 50
+  };
+
+  // adds styles.color depending on time
+  // Dynamic data
   if (hours < 12) {
     timeOfDay = "Morning";
+    styles.color = "#04756f";
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "Afternoon";
+    styles.color = "#2e0927";
   } else {
     timeOfDay = "Night";
+    styles.color = "#d90000";
   }
 
-  return <h1>Good {timeOfDay}!</h1>;
+  // practice code for lesson
+  // const styles = {
+  //   color: "#ff8c00",
+  //   backgroundColor: "#ff2d00",
+  //   fontSize: 55
+  // };
+
+  // Cleaner to make styles object
+  // If style is inline need to use {{}}
+  // Any CSS that uses a dash - , need to use camelCase
+  return <h1 style={styles}>Good {timeOfDay}!</h1>;
 };
 
 export default App;
