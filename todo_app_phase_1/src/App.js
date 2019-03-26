b1ee1944f0023e9e19202c9b1b8a98d3eee496cb
Chris <c.lamothe25@gmail.com>
2019-03-26 09:27:23 -0400

    Phase 4 of todos app: made App a class with state

--- todo_app_phase_1/src/App.js
+++ todo_app_phase_1/src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map(item => (
-    <TodoItem key={item.id} item={item} />
-  ));
-  return <div className="placeCard">{todoItems}</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} />
+    ));
+    return <div className="placeCard">{todoItems}</div>;
+  }
 }
 
 export default App;
